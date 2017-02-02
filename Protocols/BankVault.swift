f049817a3c5bce2c834c3494a7fad5dc3b1d4791
milani78 <ingakyt@gmail.com>
2017-02-02 17:02:08 -0500

    lab done

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -23,4 +23,96 @@ class BankVault {
 
 
 
+protocol ProvideAccess {
+    
+    func allowEntryWithPassword(password: [Int]) -> Bool
+    
+}
+
+
+
+
+extension BankVault: ProvideAccess {
+    
+    func allowEntryWithPassword(password: [Int]) -> Bool {
+        
+        if password.isEmpty {
+            return false
+        }
+        
+        if password.count == 0 {
+            return false
+        }
+        
+        if password.count > 10 {
+            return false
+        }        
+        
+        
+        
+//        for index in 0..<password.count{
+//            print("We have \(password[index]) left")
+//            if ((password[index+2] % 2) == 0) && ((password[0] % 2) == 0) {
+//                return true
+//            }
+//        }
+        
+        
+        for (index, element) in password.enumerated() {
+            
+            if index % 2 == 0 && element % 2 == 0 {
+                
+                print("my element: \(element)")
+                
+//                if element % 2 == 0 {
+//
+//                    print("my even int: \(element)")
+//                    
+//                    return true
+//                }
+                return true
+            }
+            
+        }
+        
+        
+//        for (index, element) in password.enumerated() {
+//            
+//            if (index % 2 != 0) && (password[0] == 0) {
+//                
+//                if (element % 2 == 0) {
+//                    
+//                    return true
+//                }
+//            }
+        
+//            if ((password[index+2] % 2) == 0) && ((password[0] % 2) == 0) {
+//                return true
+//            }
+           
+        //}
+        
+        
+        return false
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
